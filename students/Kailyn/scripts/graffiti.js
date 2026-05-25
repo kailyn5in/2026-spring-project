88cab94d3f43779e359f100fba4dd68b6c2fffe8
Kailyn <hsinlee.5in@gmail.com>
2026-05-25 11:28:19 +0800

    add function

--- students/Kailyn/scripts/graffiti.js
+++ students/Kailyn/scripts/graffiti.js
@@ -1,17 +1,44 @@
-
-//let picture= document.getElementById("dog-picture");
-//let artFrame = document.getElementById("art-frame");
-//let aart= document.getElementById("art");
+/*
+ *HTML elements
+ */
 let myGraffiti = document.getElementById("my-graffiti");
-//let button=document.getElementById("clean");
-let surface=myGraffiti.getContext("2d");
-console.log(myGraffiti);
+let surface = myGraffiti.getContext("2d");
+let cleanButton=document.getElementById("clean")
+/*
+ *Graffiti style
+ */
+surface.lineWidth = 13;
+surface.lineJoin = "round"
+surface.strokeStyle = "red";
 
+/*
+ *shapes
+ */
+function shapes() {
+    surface.rect(150, 200, 100, 100);
+    surface.stroke();
 
-//function draw(){}
+    surface.beginPath();
+    surface.moveTo(200, 50);
+    surface.lineTo(150, 200);
+    surface.closePath();
+    surface.stroke();
 
+    surface.beginPath();
+    surface.moveTo(250, 50);
+    surface.lineTo(350, 50);
+    surface.moveTo(350, 50);
+    surface.lineTo(270, 300);
+    surface.closePath();
+    surface.stroke();
 
+}
+shapes();
+/*
+ *Clean
+ */
+function Clean(){
+surface.clearRect(0,0,400,400);
+}
 
-//function Clean(){}
-
-//button.addEventListener("click", Clean);
+cleanButton.addEventListener("click", Clean);
